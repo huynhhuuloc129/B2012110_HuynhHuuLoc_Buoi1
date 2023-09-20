1f4cfa7ad2af93e560561def3a60fcaf719e2379
unknown <huynhhuuloc129@gmail.com>
2023-09-20 08:48:31 +0700

    Cai dat xu ly loi

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const contacts = require('../controller/contact.controller');
+const contacts = require('../controllers/contact.controller.js');
 
 const router = express.Router();
 
